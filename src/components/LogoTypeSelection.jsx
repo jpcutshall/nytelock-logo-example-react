cffc8a07b7b88f18302f38a7323607bec475e7f0
jpcutshall <jpcutshall@gmail.com>
2021-06-26 22:46:29 -0400

    init

---
+++ src/components/LogoTypeSelection.jsx
@@ -0,0 +1,22 @@
+import { Button } from 'semantic-ui-react'
+
+const LogoTypeSelection = () => {
+    return (
+        <div className="logo-selecetion-div">
+            
+            <div>
+                <ul>
+                    <li>HOW DO I KNOW WHICH LOGO STYLE IS RIGHT FOR ME?</li>
+                </ul>
+                <h2>Different Types of Logo Styles</h2>
+            </div>
+
+            <div>
+                <Button></Button>
+            </div>
+
+        </div>
+    )
+}
+
+export default LogoTypeSelection
